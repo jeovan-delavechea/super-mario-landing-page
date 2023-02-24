d0160f81d56e6e01c51240412cb707cf036f1eae
Jeovan Delavechea <jeovan.delavechea88@gmail.com>
2023-02-24 16:55:29 -0300

    correcao na variavel do video

--- src/js/index.js
+++ src/js/index.js
@@ -2,16 +2,16 @@ const botaoDeTrailer = document.querySelector('.botao-trailer')
 const modal = document.querySelector('.modal')
 const botaoX = document.querySelector('.fechar-modal')
 const video = document.getElementById('video')
-const linkDoVideo = video.scr;
+const linkDoVideo = video.src;
 
 function alternarModal() {modal.classList.toggle('aberto')}
 
 botaoDeTrailer.addEventListener('click', () => {
     alternarModal()
-    video.setAttribute('src', linkDoVideo)
+    video.setAttribute("src", linkDoVideo)
 })
 
 botaoX.addEventListener('click', () => {
     alternarModal()
-    video.setAttribute('src', '')
+    video.setAttribute("src", "")
 })
