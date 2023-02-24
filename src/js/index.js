e8047c59f4f7b0c33b5a16282f52673b0b8983a0
Jeovan Delavechea <jeovan.delavechea88@gmail.com>
2023-02-24 16:15:05 -0300

    add código js e menu hamburger

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+const botaoDeTrailer = document.querySelector('.botao-trailer')
+const modal = document.querySelector('.modal')
+const botaoX = document.querySelector('.fechar-modal')
+const video = document.getElementById('video')
+const linkDoVideo = video.scr;
+
+function alternarModal() {modal.classList.toggle('aberto')}
+
+botaoDeTrailer.addEventListener('click', () => {
+    alternarModal()
+    video.setAttribute('src', linkDoVideo)
+})
+
+botaoX.addEventListener('click', () => {
+    alternarModal()
+    video.setAttribute('src', '')
+})
